a1233a9bf27f062e1b5594de6ffb8d12f1d8bb82
Louis9911 <louistran090902@gmail.com>
2026-05-22 22:10:46 +0700

    update

--- tmai-saigon-backend/server.js
+++ tmai-saigon-backend/server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+console.log('URI from process.env:', process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
